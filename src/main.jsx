3de71dd7c1cea84e38554a3f4d42a7afc7b67150
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-13 17:25:55 +0600

    adding new featured

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './routers/router.jsx';
+import AuthProvider from './Auth/AuthProvide.jsx';
+
+// @ts-ignore
+ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <QueryClientProvider client={ queryClient }>
+        <div className="bg-white text-gray-700 h-full">
+          <div className="">
+            <RouterProvider router={ router } />
+          </div>
+        </div>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
